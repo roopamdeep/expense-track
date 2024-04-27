f6d8c7faf7b3726a0346ffc7db7a0caab927060e
roopamdeepk <roopam_kaur@yahoo.in>
2024-04-27 19:50:16 -0400

    expense tracker react

---
+++ src/expense-tracker/components/ExpenseFilter.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import categories from '../categories'
+
+interface Props{
+onSelectCategory: (category:string) => void
+}
+const ExpenseFilter = ({onSelectCategory}: Props) => {
+  return (
+    <div>
+      <select className="form-select" onChange={(e)=>onSelectCategory(e.target.value)}>
+        <option value="">All categories</option>
+         {categories.map(category => <option key={category} value={category}>{category}</option>)}
+      </select>
+    </div>
+  )
+}
+
+export default ExpenseFilter
